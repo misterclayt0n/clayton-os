f4dc3396b5ff2dc88700102d6489b70cef391a09
misterclayt0n <daviarantes13@gmail.com>
2024-06-18 12:54:31 -0300

    feat: add minimal vga text mode support

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"clayton os porra";
+mod vga_buffer;
 
 /// this function is called on panic.
 #[panic_handler]
@@ -14,14 +14,7 @@ fn panic(_info: &PanicInfo) -> ! {
 /// actual start of the program
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
